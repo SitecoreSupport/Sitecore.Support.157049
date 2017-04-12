103d5b8041ce5a037ba2a8912a3ec5bb768f4e00
Dmitry Kolinchuk <dmko@sitecore.net>
2017-04-12 10:57:55 +0300

    Add a workflow to an item using the Create Item save action in WFFM

---
+++ src/Sitecore.Support.157049/CreateItem.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Data;
+using Sitecore.WFFM.Abstractions.Actions;
+
+namespace Sitecore.Support.Form.Submit
+{
+    public class CreateItem : Sitecore.Form.Submit.CreateItem
+    {
+        protected override void CreateItemByFields(ID formid, AdaptedResultList fields)
+        {
+            using (new Workflows.WorkflowContextStateSwitcher(Workflows.WorkflowContextState.Enabled))
+            {
+                base.CreateItemByFields(formid, fields);
+            }
+        }
+    }
+}
