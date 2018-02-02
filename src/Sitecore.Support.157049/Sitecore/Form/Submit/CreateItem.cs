efe1b42a3b18b1fe94d8c0e43a4f0ad130a00e8b
DK\NPA <npa@sitecore.net>
2018-02-02 16:22:18 +0200

    Initial patch for 8.0.4

---
+++ src/Sitecore.Support.157049/Sitecore/Form/Submit/CreateItem.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Data;
+using Sitecore.Form.Core.Client.Data.Submit;
+using Sitecore.Workflows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Form.Submit
+{
+  public class CreateItem: Sitecore.Form.Submit.CreateItem
+  {
+    protected override void CreateItemByFields(ID formid, AdaptedResultList fields)
+    {
+      using (new WorkflowContextStateSwitcher(WorkflowContextState.Enabled))
+      {
+        base.CreateItemByFields(formid, fields);
+      }
+    }
+  }
+}
